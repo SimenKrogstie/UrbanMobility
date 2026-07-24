0318f22f58b079c7d827983b0e8f7a656a6a3122
SimenKrogstie <SimenKrogstie@gmail.com>
2026-07-24 15:25:46 +0200

    feat: Added tests for import of package

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,4 +1,6 @@
-def test_import():
-    import urbanmobility
+import urbanmobility
 
+def test_public_api():
     assert urbanmobility.__version__ == "0.1.0"
+    assert urbanmobility.DEFAULT_CRS is not None
+    assert urbanmobility.CRS_WGS84 is not None
