714f2504109412f584cfee6135c0004197342dd0
SimenKrogstie <SimenKrogstie@gmail.com>
2026-07-24 16:59:36 +0200

    refactor: expose buildings public API

--- tests/test_buildings.py
+++ tests/test_buildings.py
@@ -1,12 +1,17 @@
 def test_buildings_public_api():
-    from urbanmobility.buildings import (
-        fetch_buildings,
-        calculate_building_indicators,
-        plot_building_indicators,
-        plot_building_types,
-    )
+    from urbanmobility import buildings
 
-    assert callable(fetch_buildings)
-    assert callable(calculate_building_indicators)
-    assert callable(plot_building_indicators)
-    assert callable(plot_building_types)
+    assert callable(buildings.fetch_buildings)
+    assert callable(buildings.calculate_building_indicators)
+    assert callable(buildings.plot_building_indicators)
+    assert callable(buildings.plot_building_types)
+
+def test_buildings_exports():
+    from urbanmobility import buildings
+
+    assert set(buildings.__all__) == {
+        "fetch_buildings",
+        "calculate_building_indicators",
+        "plot_building_indicators",
+        "plot_building_types",
+    }
