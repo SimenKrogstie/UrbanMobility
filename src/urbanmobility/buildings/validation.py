dc83ef6de040f5b540e74f1d86ea31c4976afbe5
SimenKrogstie <SimenKrogstie@gmail.com>
2026-07-28 21:53:30 +0200

    feat: Helper function for validating input data

--- src/urbanmobility/buildings/validation.py
+++ src/urbanmobility/buildings/validation.py
@@ -1,38 +1,42 @@
+"""Check input assumptions"""
+
 import geopandas as gpd
 
-
-def validate_buildings_data(
+def validate_building_data(
         buildings: gpd.GeoDataFrame,
         districts: gpd.GeoDataFrame,
         district_col: str,
 ) -> None:
-
-
+    
     for name, gdf in [
         ("buildings", buildings),
         ("districts", districts),
     ]:
-        if not isinstance(gpd.GeoDataFrame):
+        if not isinstance(gdf, gpd.GeoDataFrame):
             raise TypeError(
                 f"{name} must be a GeoDataFrame"
             )
-
+        
+        if gdf.crs is None:
+            raise ValueError(
+                f"{name} has no CRS"
+            )
+        
         if gdf.geometry.is_empty.any():
             raise ValueError(
                 f"{name} contains empty geometries"
             )
 
-        if gdf.crs is None:
+        if not gdf.geometry.is_valid.all():
             raise ValueError(
-                f"{name} has no CRS"
+                f"{name} contains invalid geometries"
             )
 
-    if district_col not in buildings.columns:
-        raise KeyError(
-            f"{district_col!r} missing from buildings."
-        )
-
-    if district_col not in districts.columns:
-        raise KeyError(
-            f"{district_col!r} missing from districts."
-        )
+        for gdf, name in [
+            (buildings, "buildings"),
+            (districts, "districts")
+        ]: 
+            if district_col not in gdf.columns:
+                raise KeyError(
+                    f"{district_col!r} missing from {name}"
+                )
