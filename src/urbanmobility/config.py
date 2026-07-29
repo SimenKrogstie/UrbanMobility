e47f809f17babaec8c79cf128f437cad9d382067
SimenKrogstie <SimenKrogstie@gmail.com>
2026-07-29 22:48:55 +0200

    feat: Moved constant for buildings here

--- src/urbanmobility/config.py
+++ src/urbanmobility/config.py
@@ -11,5 +11,16 @@ RAW_DATA = {
     "districts": DATA_DIR / "raw"/"oslo_bydeler_befolkning_2024.geojson",
 }
 
+# CRS constants
 DEFAULT_CRS = "EPSG:28533"
 CRS_WGS84 = "EPSG:4326"
+
+# Buildings constants
+AREA_M2 = "area_m2"
+AREA_KM2 = "area_km2"
+
+BUILDING_COUNT = "num_buildings"
+BUILDING_AREA = "building_area_m2"
+BUILDING_DENSITY = "buildings_per_km2"
+BUILT_AREA_PERCENT = "built_up_area_percent"
+AVG_BUILDING_AREA = "avg_building_area_m2"
